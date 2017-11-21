f8ddf52a2d92af67f95d14d8b318a34505561b57
Theo H <Theo@Harkenbusch.com>
2017-11-21 21:19:21 +0100

    added config file with constants

---
+++ common/src/main/java/com/theovier/democoin/common/Config.java
@@ -0,0 +1,11 @@
+package com.theovier.democoin.common;
+
+public abstract class Config {
+
+    public static final int COINBASE_REWARD = 50;
+    public static final int COINBASE_MATURITY = 1;
+    public static final int MAX_TRANSACTIONS_PER_BLOCK = 5;
+
+
+
+}
