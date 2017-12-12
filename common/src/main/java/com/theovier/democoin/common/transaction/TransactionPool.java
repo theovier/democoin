4ccae8250d81d5018c90bc34195c5f6cb7733af0
Theo H <Theo@Harkenbusch.com>
2017-12-12 14:04:07 +0100

    add

---
+++ common/src/main/java/com/theovier/democoin/common/transaction/TransactionPool.java
@@ -0,0 +1,24 @@
+package com.theovier.democoin.common.transaction;
+
+import java.util.*;
+
+public class TransactionPool {
+
+    private static Set<Transaction> pendingTransactions = new HashSet<>();
+
+    public static synchronized boolean add(Transaction transaction) {
+        if (TransactionValidator.validate(transaction)) {
+            pendingTransactions.add(transaction);
+            return true;
+        }
+        return false;
+    }
+
+    public static synchronized void remove(Transaction transaction) {
+        pendingTransactions.remove(transaction);
+    }
+
+    public static boolean containsAll(Collection<Transaction> transactions) {
+        return pendingTransactions.containsAll(transactions);
+    }
+}
