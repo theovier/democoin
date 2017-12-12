cc9385749cb3463296d4881360d2d265ba241fa7
Theo H <Theo@Harkenbusch.com>
2017-12-12 16:18:59 +0100

    added blockvalidator

---
+++ common/src/main/java/com/theovier/democoin/common/BlockValidator.java
@@ -0,0 +1,80 @@
+package com.theovier.democoin.common;
+
+import com.theovier.democoin.common.transaction.Transaction;
+import com.theovier.democoin.common.transaction.TransactionPool;
+import com.theovier.democoin.common.transaction.TransactionValidator;
+
+public class BlockValidator {
+
+    public static boolean isValid(Block candidate, Block prevBlock) {
+        if (!hasValidIndex(candidate, prevBlock)) {
+            return false;
+        }
+        if (!hasValidHashChain(candidate, prevBlock)) {
+            return false;
+        }
+        if (!hasValidMerkleRoot(candidate)) {
+            return false;
+        }
+        if (!hasValidBlockHash(candidate)) {
+            return false;
+        }
+        if (!hasValidProofOfWork(candidate)) {
+            //return false;
+        }
+        if (!hasValidTransactionCount(candidate)) {
+           //return false;
+        }
+        if (!hasOnlyValidTransactions(candidate)) {
+            return false;
+        }
+        if (!hasOnlyBroadcastedTransactions(candidate)) {
+            //return false;
+        }
+        if (!hasCoinbaseTX(candidate)) {
+           // return false;
+        }
+        return true;
+    }
+
+    public static boolean hasValidIndex(Block candidate, Block prevBlock) {
+        return (candidate.getIndex() == prevBlock.getIndex() + 1);
+    }
+
+    public static boolean hasValidHashChain(Block candidate, Block prevBlock) {
+        return prevBlock.getHash().equals(candidate.getPreviousBlockHash());
+    }
+
+    public static boolean hasValidMerkleRoot(Block candidate) {
+        return candidate.computeMerkleRoot().equals(candidate.getMerkleRoot());
+    }
+
+    public static boolean hasValidBlockHash(Block candidate) {
+        return candidate.computeHash().equals(candidate.getHash());
+    }
+
+    public static boolean hasValidProofOfWork(Block candidate) {
+        return false;
+    }
+
+    public static boolean hasValidTransactionCount(Block candidate) {
+        return candidate.getTransactions().size() <= Config.MAX_TRANSACTIONS_PER_BLOCK;
+    }
+
+    public static boolean hasOnlyValidTransactions(Block candidate) {
+        return candidate.getTransactions()
+                .stream()
+                .anyMatch(TransactionValidator::validate);
+    }
+
+    public static boolean hasOnlyBroadcastedTransactions(Block candidate) {
+        return TransactionPool.containsAll(candidate.getTransactions());
+    }
+
+    public static boolean hasCoinbaseTX(Block candidate) {
+        return candidate.getTransactions()
+                .stream()
+                .filter(Transaction::isCoinBase)
+                .count() == 1;
+    }
+}
---
+++ common/src/test/java/com/theovier/democoin/common/BlockValidatorTest.java
@@ -0,0 +1,53 @@
+package com.theovier.democoin.common;
+
+import com.theovier.democoin.common.transaction.CoinbaseTransaction;
+import com.theovier.democoin.common.transaction.Transaction;
+import org.junit.Test;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class BlockValidatorTest {
+
+    @Test
+    public void testValidIndex() {
+        Blockchain blockchain = setupBlockchain();
+        Block block = new Block(blockchain.getLastBlock(),  new ArrayList<>(), 0);
+        assert BlockValidator.hasValidIndex(block, blockchain.getLastBlock());
+    }
+
+    @Test
+    public void testValidHashChain() {
+        Blockchain blockchain = setupBlockchain();
+        Block block = new Block(blockchain.getLastBlock(),  new ArrayList<>(), 0);
+        assert BlockValidator.hasValidHashChain(block, blockchain.getLastBlock());
+    }
+
+    @Test
+    public void testValidTransactionCount() {
+        List<Transaction> txs = new ArrayList<>();
+        for (int i=0; i <= Config.MAX_TRANSACTIONS_PER_BLOCK; i++) {
+           txs.add(new CoinbaseTransaction(new Address("...")));
+        }
+        Blockchain blockchain = setupBlockchain();
+        Block block = new Block(blockchain.getLastBlock(), txs, 0);
+        assert !BlockValidator.hasValidTransactionCount(block);
+    }
+
+    @Test
+    public void testHasCoinbaseTX() {
+        List<Transaction> txs = new ArrayList<>();
+        txs.add(new CoinbaseTransaction(new Address("...")));
+        txs.add(new CoinbaseTransaction(new Address("...")));
+        Blockchain blockchain = setupBlockchain();
+        Block block = new Block(blockchain.getLastBlock(), txs, 0);
+        assert !BlockValidator.hasCoinbaseTX(block);
+    }
+
+    public Blockchain setupBlockchain() {
+        Blockchain blockchain = new Blockchain();
+        Block genesisBlock = Block.generateGenesisBlock();
+        blockchain.add(genesisBlock);
+        return blockchain;
+    }
+}
