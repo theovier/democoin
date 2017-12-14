bb31f80a8a706f9f9a2d88cf0b54e301b80d71e5
Theo H <Theo@Harkenbusch.com>
2017-12-14 11:08:28 +0100

    mini refactoring. added todo

--- common/src/main/java/com/theovier/democoin/common/transaction/TransactionValidator.java
+++ common/src/main/java/com/theovier/democoin/common/transaction/TransactionValidator.java
@@ -1,10 +1,9 @@
 package com.theovier.democoin.common.transaction;
 
-import com.theovier.democoin.common.Config;
-
 public class TransactionValidator {
 
     //todo implement more checks.
+    //todo rename methods to match BlockValidator
 
     /**
      * Adapt to https://en.bitcoin.it/wiki/Protocol_rules#.22tx.22_messages
@@ -28,6 +27,13 @@ public class TransactionValidator {
     }
 
     private static boolean validateRegularTx(Transaction tx) {
+        if (!validateRegularTxInputs(tx)) {
+            return false;
+        }
+        return true;
+    }
+
+    private static boolean validateRegularTxInputs(Transaction tx) {
         for (TxInput in : tx.getInputs()) {
             if (!validateRegularTxInput(in)) {
                 return false;
