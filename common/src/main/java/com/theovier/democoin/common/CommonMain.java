8d5e2329bb7c1ace722753095ee316c9e11a496d
Theo H <Theo@Harkenbusch.com>
2017-12-14 16:44:07 +0100

    removed unused methods

--- common/src/main/java/com/theovier/democoin/common/CommonMain.java
+++ common/src/main/java/com/theovier/democoin/common/CommonMain.java
@@ -1,18 +1,14 @@
 package com.theovier.democoin.common;
 
-import org.apache.log4j.Logger;
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
 
 import java.security.Security;
 
 public class CommonMain {
 
-    private static final Logger LOG = Logger.getLogger(CommonMain.class);
-
-    public static void main (String[] args) throws Exception {
+    public static void main (String[] args) {
         Security.addProvider(new BouncyCastleProvider());
         Demo miner = new Demo();
-        //miner.generateDemoBlockchainFile();
         miner.demoBlockchain();
     }
 }
--- common/src/main/java/com/theovier/democoin/common/Demo.java
+++ common/src/main/java/com/theovier/democoin/common/Demo.java
@@ -1,58 +1,16 @@
 package com.theovier.democoin.common;
 
 import com.theovier.democoin.common.crypto.Sha256Hash;
-import com.theovier.democoin.common.transaction.CoinbaseTransaction;
-import com.theovier.democoin.common.transaction.Transaction;
-import com.theovier.democoin.common.transaction.TransactionValidator;
-import com.theovier.democoin.common.transaction.UTXOPool;
+import com.theovier.democoin.common.transaction.*;
 import org.apache.log4j.Logger;
 
 import java.security.KeyPair;
-import java.util.ArrayList;
-import java.util.List;
 
 
 public class Demo {
 
     private static final Logger LOG = Logger.getLogger(Demo.class);
     private Blockchain blockchain = new Blockchain();
-
-    public void demoTransactions() {
-        Wallet wallet = new Wallet();
-        KeyPair keypair = wallet.getKeyPair();
-
-        Address target = Address.generateAddress(keypair.getPublic());
-        Transaction coinbaseTx = new CoinbaseTransaction(target);
-        UTXOPool.add(coinbaseTx);
-
-        Transaction tx1 = new Transaction(".");
-        tx1.addInput(coinbaseTx.getFirstOutput());
-        tx1.signInput(0, keypair);
-        tx1.build();
-
-        Transaction tx2 = new Transaction(".");
-        tx2.addInput(coinbaseTx.getFirstOutput());
-        tx2.signInput(0, keypair);
-        tx2.build();
-
-        LOG.info(coinbaseTx);
-        LOG.info(tx1);
-        LOG.info(tx2);
-        LOG.info(TransactionValidator.validate(tx1));
-        LOG.info(TransactionValidator.validate(tx2));
-    }
-
-    public void generateDemoBlockchainFile() {
-        //remove blockchain.dat before calling
-        // -> only 1 genesis block with 1 coinbase TX is present
-        //because of the blockchain = new Blockchain();
-
-//        blockchain.save();
-        Block block = new Block(blockchain.getLastBlock(),  0, new Address("..."), new ArrayList<>());
-        blockchain.append(block);
-        LOG.info(blockchain);
-        UTXOPool.compute(blockchain);
-    }
 
     public void demoBlockchain() {
         UTXOPool.compute(blockchain);
@@ -64,13 +22,17 @@ public class Demo {
         Transaction tx1 = new Transaction(".");
 
         //reference the genesisBlock coinbase transaction.
-        tx1.addInput(new Sha256Hash("cb21e6d3a2fdbe879ffcb7ea3d19a50410755c4456a35ae8561be4573ff743de"), 0);
+        TxInput input1 = new TxInput(new Sha256Hash("825ebc967d94b6ddab65d241a19cfa5b916f86d153f28502fe8aca77f5dd84ad"), 0, 50);
+        tx1.addInput(input1);
         tx1.addOutput(target, 20);
+        tx1.addOutput(target, 20);
+
         tx1.signInput(0, keypair);
         tx1.build();
 
         Block block = new Block(blockchain.getLastBlock(),  0, target, tx1);
         blockchain.append(block);
-        blockchain.save();
+        LOG.info(blockchain);
+//        blockchain.save();
     }
 }
