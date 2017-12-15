f6edb166e702520007391ac0dcd589d35ec7ef2f
Theo H <Theo@Harkenbusch.com>
2017-12-15 10:57:21 +0100

    added check if text length is > 255 before cutting

--- common/src/main/java/com/theovier/democoin/common/Utils.java
+++ common/src/main/java/com/theovier/democoin/common/Utils.java
@@ -2,9 +2,11 @@ package com.theovier.democoin.common;
 
 public class Utils {
 
-
     public static String escapeText(final String text) {
-        String escaped = text.substring(0, 255);
+        String escaped = text;
+        if (text.length() > 255) {
+            escaped = text.substring(0, 255);
+        }
         escaped.replace(">", ")");
         escaped.replace("<", "(");
         return text;
