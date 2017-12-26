eb4e3d8d073ee892c7e4c4eeee48c6996ca592d1
theovier <Theo@Harkenbusch.com>
2017-12-26 22:42:36 +0100

    added try catch

--- node/src/main/java/com/theovier/democoin/node/NodeMain.java
+++ node/src/main/java/com/theovier/democoin/node/NodeMain.java
@@ -2,12 +2,20 @@ package com.theovier.democoin.node;
 
 import com.theovier.democoin.node.network.Node;
 
+import java.io.IOException;
+
 public class NodeMain {
 
     private static final org.apache.log4j.Logger LOG = org.apache.log4j.Logger.getLogger(NodeMain.class);
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args)  {
+
         Node node = new Node();
-        node.start();
+        try {
+            node.start();
+        } catch (IOException e) {
+            node.shutdown();
+        }
+
     }
 }
