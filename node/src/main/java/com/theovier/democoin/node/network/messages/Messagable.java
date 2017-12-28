84fc6d343507d57b632571386999e84e0bd8da14
Theo H <Theo@Harkenbusch.com>
2017-12-28 10:04:00 +0100

    removed interface permanently

--- node/src/main/java/com/theovier/democoin/node/network/messages/Messagable.java
+++
@@ -1,9 +0,0 @@
-package com.theovier.democoin.node.network.messages;
-
-import com.theovier.democoin.node.network.Peer;
-
-import java.io.IOException;
-
-public interface Messagable {
-    void handle(Peer receiver) throws IOException;
-}
