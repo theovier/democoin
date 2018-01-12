9105ad3ef67b96a86f5dade22bcd80c990efdbfd
Theo H <Theo@Harkenbusch.com>
2018-01-12 20:05:14 +0100

    fixed a bug that the public key could not be loaded from the blockchain.xml which lead to invalid transactionInputs because the pubKey was null. Added security provider, that fixed the issue.

--- node/src/main/java/com/theovier/democoin/node/NodeMain.java
+++ node/src/main/java/com/theovier/democoin/node/NodeMain.java
@@ -2,15 +2,17 @@ package com.theovier.democoin.node;
 
 import com.theovier.democoin.common.Blockchain;
 import com.theovier.democoin.node.network.Node;
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
 
 import java.io.IOException;
+import java.security.Security;
 
 public class NodeMain {
 
     private static final org.apache.log4j.Logger LOG = org.apache.log4j.Logger.getLogger(NodeMain.class);
 
     public static void main(String[] args)  {
-
+        Security.addProvider(new BouncyCastleProvider());
         Blockchain blockchain = Blockchain.loadFromDisc();
         Node node = new Node(blockchain);
         try {
