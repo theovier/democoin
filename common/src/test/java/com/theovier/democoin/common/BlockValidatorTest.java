61f97cba6409bd9a21c847fcc8f77b6bd3e0174c
Theo H <Theo@Harkenbusch.com>
2018-01-14 09:01:53 +0100

    added first tests for BlockValidator

--- common/src/test/java/com/theovier/democoin/common/BlockValidatorTest.java
+++ common/src/test/java/com/theovier/democoin/common/BlockValidatorTest.java
@@ -1,51 +1,99 @@
 package com.theovier.democoin.common;
 
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
+import java.io.File;
 import java.io.IOException;
+import java.security.Security;
 
-import static org.junit.jupiter.api.Assertions.*;
-
+/**
+ * a block can only be validated in the context of a blockchain.
+ * the test resources contain blockchains which are either valid
+ * or invalid. The invalid ones always contain the error in the
+ * last block, so we can access it by calling chain.getLastBlock().
+ * These tests aim to detect invalid candidates to proof they are
+ * functional.
+ */
 class BlockValidatorTest {
+
+    @BeforeEach
+    void setUp() {
+        Security.addProvider(new BouncyCastleProvider());
+    }
 
     @Test
     void isValid() throws IOException {
-
+        File xmlFile = new File(getClass().getResource("/validBlockchains/valid_with_1_transaction.xml").getFile());
+        Blockchain blockchain = Blockchain.loadFromFile(xmlFile);
+        assertTrue(blockchain.isValid());
     }
 
     @Test
-    void hasValidProofOfWork() {
+    void hasValidProofOfWork() throws IOException {
+        File file = new File(getClass().getResource("/invalidBlockchains/invalid_pow.xml").getFile());
+        Blockchain invalidBlockchain = Blockchain.loadFromFile(file);
+        Block invalidBlock = invalidBlockchain.get(1);
+        assertFalse(BlockValidator.hasValidProofOfWork(invalidBlock, invalidBlockchain));
     }
 
     @Test
-    void hasValidIndex() {
+    void hasValidIndex() throws IOException {
+        File file = new File(getClass().getResource("/invalidBlockchains/invalid_index.xml").getFile());
+        Blockchain invalidBlockchain = Blockchain.loadFromFile(file);
+        Block prevBlock = invalidBlockchain.get(0);
+        Block invalidBlock = invalidBlockchain.get(1);
+        assertFalse(BlockValidator.hasValidIndex(invalidBlock, prevBlock));
     }
 
     @Test
-    void hasValidHashChain() {
+    void hasValidHashChain() throws IOException {
+        File file = new File(getClass().getResource("/invalidBlockchains/invalid_hashchain.xml").getFile());
+        Blockchain invalidBlockchain = Blockchain.loadFromFile(file);
+        Block prevBlock = invalidBlockchain.get(0);
+        Block invalidBlock = invalidBlockchain.get(1);
+        assertFalse(BlockValidator.hasValidHashChain(invalidBlock, prevBlock));
     }
 
     @Test
-    void hasValidMerkleRoot() {
+    void hasValidBlockHash() throws IOException {
+        File file = new File(getClass().getResource("/validBlockchains/valid_with_1_transaction.xml").getFile());
+        Blockchain validBlockchain = Blockchain.loadFromFile(file);
+        Block validBlock = validBlockchain.get(1);
+        assertTrue(BlockValidator.hasValidBlockHash(validBlock));
     }
 
     @Test
-    void hasValidBlockHash() {
+    void hasValidMerkleRoot() throws IOException {
+        File file = new File(getClass().getResource("/invalidBlockchains/invalid_merkleRoot.xml").getFile());
+        Blockchain invalidBlockchain = Blockchain.loadFromFile(file);
+        Block invalidBlock = invalidBlockchain.get(1);
+        assertFalse(BlockValidator.hasValidMerkleRoot(invalidBlock));
     }
 
     @Test
-    void hasValidTransactionCount() {
+    void hasValidTransactionCount() throws IOException {
+        File file = new File(getClass().getResource("/validBlockchains/valid_with_1_transaction.xml").getFile());
+        Blockchain validBlockchain = Blockchain.loadFromFile(file);
+        Block validBlock = validBlockchain.get(1);
+        assertTrue(BlockValidator.hasValidTransactionCount(validBlock));
     }
 
     @Test
-    void hasOnlyValidTransactions() {
+    void hasOnlyValidTransactions() throws IOException {
+        //todo
     }
 
     @Test
-    void hasCoinbaseTx() {
+    void hasCoinbaseTx() throws IOException {
+        //todo
     }
 
     @Test
-    void hasValidCoinbaseTx() {
+    void hasValidCoinbaseTx() throws IOException {
+        //todo
     }
 }
