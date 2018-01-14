bdb411f73437c8bbd0db2756b4883c8431ca9c3d
Theo H <Theo@Harkenbusch.com>
2018-01-14 07:57:31 +0100

    added junit5 stub

--- common/src/test/java/com/theovier/democoin/common/BlockValidatorTest.java
+++ common/src/test/java/com/theovier/democoin/common/BlockValidatorTest.java
@@ -1,55 +1,51 @@
 package com.theovier.democoin.common;
 
-import com.theovier.democoin.common.transaction.CoinbaseTransaction;
-import com.theovier.democoin.common.transaction.Transaction;
-import org.junit.Test;
-import java.util.ArrayList;
-import java.util.List;
+import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
 
-public class BlockValidatorTest {
+import static org.junit.jupiter.api.Assertions.*;
 
-    /*
+class BlockValidatorTest {
+
     @Test
-    public void testValidIndex() {
-        Blockchain blockchain = setupBlockchain();
-        Block block = new Block(blockchain.getLastBlock(),  0, "", new Address(""), new ArrayList<>());
-        assert BlockValidator.hasValidIndex(block, blockchain.getLastBlock());
+    void isValid() throws IOException {
+
     }
 
     @Test
-    public void testValidHashChain() {
-        Blockchain blockchain = setupBlockchain();
-        Block block = new Block(blockchain.getLastBlock(), 0, "", new Address(""),  new ArrayList<>());
-        assert BlockValidator.hasValidHashChain(block, blockchain.getLastBlock());
+    void hasValidProofOfWork() {
     }
 
     @Test
-    public void testValidTransactionCount() {
-        List<Transaction> txs = new ArrayList<>();
-        for (int i = 0; i <= ConsensusParams.MAX_TRANSACTIONS_PER_BLOCK; i++) {
-           txs.add(new CoinbaseTransaction(new Address("...")));
-        }
-        Blockchain blockchain = setupBlockchain();
-        Block block = new Block(blockchain.getLastBlock(),  0,"", new Address(""), txs);
-        assert !BlockValidator.hasValidTransactionCount(block);
+    void hasValidIndex() {
     }
 
     @Test
-    public void testHasCoinbaseTX() {
-        List<Transaction> txs = new ArrayList<>();
-        txs.add(new CoinbaseTransaction(new Address("...")));
-        txs.add(new CoinbaseTransaction(new Address("...")));
-        Blockchain blockchain = setupBlockchain();
-        Block block = new Block(blockchain.getLastBlock(), 0, "", new Address(""),  new ArrayList<>());
-        assert !BlockValidator.hasCoinbaseTx(block);
+    void hasValidHashChain() {
     }
 
-    public Blockchain setupBlockchain() {
-        Blockchain blockchain = new Blockchain();
-        Block genesisBlock = Block.generateGenesisBlock();
-        blockchain.append(genesisBlock);
-        return blockchain;
+    @Test
+    void hasValidMerkleRoot() {
     }
-    */
+
+    @Test
+    void hasValidBlockHash() {
+    }
+
+    @Test
+    void hasValidTransactionCount() {
+    }
+
+    @Test
+    void hasOnlyValidTransactions() {
+    }
+
+    @Test
+    void hasCoinbaseTx() {
+    }
+
+    @Test
+    void hasValidCoinbaseTx() {
+    }
 }
