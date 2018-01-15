33f97dddc585847a8f62390b4bcc7ae4cba32b32
Theo H <Theo@Harkenbusch.com>
2018-01-15 20:06:43 +0100

    added laptop's ip address to the default hosts

--- node/src/main/java/com/theovier/democoin/node/network/NetworkParams.java
+++ node/src/main/java/com/theovier/democoin/node/network/NetworkParams.java
@@ -8,6 +8,7 @@ public abstract class NetworkParams {
     public static final int MAX_IN_CONNECTIONS = 10;
     public static final int MAX_CONNECTIONS = MAX_IN_CONNECTIONS + MAX_OUT_CONNECTIONS;
     public static final String[] DEFAULT_HOSTS = new String[] {
-            "192.168.1.48"
+            "192.168.1.48",
+            "192.168.1.47"
     };
 }
