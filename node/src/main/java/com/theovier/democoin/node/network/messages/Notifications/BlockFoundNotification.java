e1ccaeae185636e5a87853c7aedafe22f2e1900e
Theo H <Theo@Harkenbusch.com>
2018-01-26 13:31:48 +0100

    added implementation

--- node/src/main/java/com/theovier/democoin/node/network/messages/Notifications/BlockFoundNotification.java
+++ node/src/main/java/com/theovier/democoin/node/network/messages/Notifications/BlockFoundNotification.java
@@ -1,13 +1,17 @@
 package com.theovier.democoin.node.network.messages.Notifications;
 
 import com.theovier.democoin.common.Block;
+import com.theovier.democoin.common.Blockchain;
 import com.theovier.democoin.node.network.Peer;
 import org.apache.log4j.Logger;
+
+import java.io.IOException;
 
 
 public final class BlockFoundNotification extends Notification {
 
     private static final Logger LOG = Logger.getLogger(BlockFoundNotification.class);
+    private static final long serialVersionUID = -6256195613395593467L;
     private final Block foundBlock;
 
     public BlockFoundNotification(final Block block) {
@@ -16,7 +20,26 @@ public final class BlockFoundNotification extends Notification {
 
     @Override
     public void handle(Peer receiver) {
-        boolean appended = receiver.getBlockchain().append(foundBlock);
-        LOG.info(appended);
+        Blockchain blockchain = receiver.getBlockchain();
+        long index = foundBlock.getIndex();
+        long currentIndex = blockchain.getHeight() - 1;
+        if (index > currentIndex + 1) {
+            downloadLongerBlockchain(receiver, blockchain);
+        } else if (blockchain.append(foundBlock)) {
+            blockchain.saveToDisc();
+            receiver.broadcast(this);
+            LOG.info("received valid block... appended...");
+        }
+    }
+
+    private void downloadLongerBlockchain(Peer peer, Blockchain current) {
+        try {
+            Blockchain other = peer.requestBlockchain();
+            if (current.substitute(other)) {
+                current.saveToDisc();
+            }
+        } catch (IOException | InterruptedException e) {
+            LOG.error(e);
+        }
     }
 }
