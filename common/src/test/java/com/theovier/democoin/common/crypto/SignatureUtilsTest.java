8f054aaf186ce065780ad35e6a2434e960264b5a
Theo H <Theo@Harkenbusch.com>
2018-01-31 23:32:41 +0100

    added more tests

--- common/src/test/java/com/theovier/democoin/common/crypto/SignatureUtilsTest.java
+++ common/src/test/java/com/theovier/democoin/common/crypto/SignatureUtilsTest.java
@@ -1,13 +1,11 @@
 package com.theovier.democoin.common.crypto;
 
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
+import org.bouncycastle.util.encoders.Hex;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.security.GeneralSecurityException;
-import java.security.KeyPair;
-import java.security.PublicKey;
-import java.security.Security;
+import java.security.*;
 import java.security.interfaces.ECPrivateKey;
 
 import static com.theovier.democoin.common.crypto.SignatureUtils.generateKeyPair;
@@ -35,4 +33,42 @@ class SignatureUtilsTest {
         assertEquals(pair.getPublic(), copy.getPublic());
         assertEquals(pair.getPrivate(), copy.getPrivate());
     }
+
+    @Test
+    void getPublicKey() throws GeneralSecurityException {
+        KeyPair pair = generateKeyPair();
+        PublicKey publicKey = pair.getPublic();
+        byte[] x509key = publicKey.getEncoded();
+        PublicKey copy = SignatureUtils.getPublicKey(x509key);
+        assertEquals(publicKey, copy);
+    }
+
+    @Test
+    void getPrivateKey() throws GeneralSecurityException {
+        KeyPair pair = generateKeyPair();
+        PrivateKey privateKey = pair.getPrivate();
+        byte[] pkcs8key = privateKey.getEncoded();
+        PrivateKey copy = SignatureUtils.getPrivateKey(pkcs8key);
+        assertEquals(privateKey, copy);
+    }
+
+    @Test
+    void getPublicKeyOrNull() throws GeneralSecurityException {
+        KeyPair pair = generateKeyPair();
+        PublicKey publicKey = pair.getPublic();
+        String publicKeyHex = Hex.toHexString(publicKey.getEncoded());
+        PublicKey copy = SignatureUtils.getPublicKeyOrNull(publicKeyHex);
+        assertEquals(publicKey, copy);
+    }
+
+    @Test
+    void sign() throws GeneralSecurityException {
+
+    }
+
+    @Test
+    void verify() throws GeneralSecurityException {
+
+    }
+
 }
