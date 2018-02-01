bf2f6219ef0544da1254c086e8ddeca0e1adffb7
Theo H <Theo@Harkenbusch.com>
2018-02-01 20:20:03 +0100

    updated demo method to work with the latest wallet

--- common/src/main/java/com/theovier/democoin/common/Demo.java
+++ common/src/main/java/com/theovier/democoin/common/Demo.java
@@ -1,10 +1,14 @@
 package com.theovier.democoin.common;
 
 import com.theovier.democoin.common.crypto.Sha256Hash;
+import com.theovier.democoin.common.io.Wallet;
 import com.theovier.democoin.common.transaction.Transaction;
 import com.theovier.democoin.common.transaction.TxInput;
 import org.apache.log4j.Logger;
 
+import java.io.File;
+import java.io.IOException;
+import java.security.GeneralSecurityException;
 import java.security.KeyPair;
 
 
@@ -15,25 +19,27 @@ public class Demo {
 
     public void demoMining() {
 
-
-        LOG.info(blockchain.isValid() + ", " + blockchain.getHeight());
-
-
         Wallet wallet = new Wallet();
-        KeyPair keypair = wallet.getKeyPair();
+        KeyPair keypair = null;
+        try {
+            File file = new File("1AVuQjcnquXEaXgggJx7TsyMBjbatiBtNB.key");
+            keypair = wallet.loadKeyPair(file);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (GeneralSecurityException e) {
+            e.printStackTrace();
+        }
+
         Address target = Address.generateAddress(keypair.getPublic());
 
-        Transaction tx1 = new Transaction("my message");
-        TxInput input1 = new TxInput(new Sha256Hash("9d359f05f53053aaa0618d4f0894abd364b94dcc4d18b0fd937244dc49f646bf"), 0);
+        Transaction tx1 = new Transaction("broadcast tx");
+        TxInput input1 = new TxInput(new Sha256Hash("986afc4e482ff3fec2527cfa1140e2991bc500aa4022b30a4e8719f5e1eebb71"), 0);
         tx1.addInput(input1);
-        tx1.addOutput(target, 70);
-        tx1.signInput(0, keypair);
+        tx1.addOutput(target, 130);
+        input1.sign(keypair);
+        //tx1.signInput(0, keypair);
         tx1.build();
 
         LOG.info(blockchain.addToMemPool(tx1));
-
-        Miner miner = new Miner(blockchain, target, "mined by theo's computer");
-        miner.start();
-
     }
 }
