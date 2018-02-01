91152eab7c00eef93ebd833e386f328bc89946a1
Theo H <Theo@Harkenbusch.com>
2018-02-01 12:13:47 +0100

    removed static main methods

--- common/src/main/java/com/theovier/democoin/common/crypto/SignatureUtils.java
+++ common/src/main/java/com/theovier/democoin/common/crypto/SignatureUtils.java
@@ -1,30 +1,18 @@
 package com.theovier.democoin.common.crypto;
 
-import org.apache.log4j.Logger;
-import org.bouncycastle.asn1.eac.ECDSAPublicKey;
-import org.bouncycastle.crypto.KeyGenerationParameters;
-import org.bouncycastle.crypto.generators.ECKeyPairGenerator;
-import org.bouncycastle.crypto.params.ECDomainParameters;
+
 import org.bouncycastle.jce.ECNamedCurveTable;
-import org.bouncycastle.jce.provider.BouncyCastleProvider;
 import org.bouncycastle.jce.spec.ECParameterSpec;
 import org.bouncycastle.jce.spec.ECPublicKeySpec;
 import org.bouncycastle.math.ec.ECPoint;
-import org.bouncycastle.math.ec.FixedPointCombMultiplier;
 import org.bouncycastle.util.encoders.Hex;
 
-import java.math.BigInteger;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.security.*;
 import java.security.interfaces.ECPrivateKey;
-import java.security.interfaces.ECPublicKey;
 import java.security.spec.PKCS8EncodedKeySpec;
 import java.security.spec.X509EncodedKeySpec;
 
 public class SignatureUtils {
-
-    private static final Logger LOG = Logger.getLogger(SignatureUtils.class);
 
     public static KeyPair generateKeyPair() throws GeneralSecurityException {
         ECParameterSpec ecSpec = ECNamedCurveTable.getParameterSpec("secp256r1");
@@ -108,60 +96,6 @@ public class SignatureUtils {
         } catch (GeneralSecurityException e) {
             return null;
         }
-    }
-
-
-
-    public static void main9(String[] args) throws Exception {
-        Security.addProvider(new BouncyCastleProvider());
-
-        //generate and save keys.
-        KeyPair pair = generateKeyPair();
-        PublicKey pub = pair.getPublic();
-        PrivateKey priv = pair.getPrivate();
-        byte[] publicKeyBytes = pub.getEncoded();
-        byte[] privateKeyBytes = priv.getEncoded();
-        Files.write(Paths.get("key.priv"), privateKeyBytes);
-        Files.write(Paths.get("key.pub"), publicKeyBytes);
-
-        byte[] unsignedData = "SIGN ME".getBytes("UTF-8");
-        byte[] signedData = sign(unsignedData, privateKeyBytes);
-        boolean isValid = verify(signedData, publicKeyBytes, unsignedData);
-        LOG.info("signed data: " + Hex.toHexString(signedData));
-        LOG.info(isValid);
-
-        //read from file and verify previous signature.
-        publicKeyBytes = Files.readAllBytes(Paths.get("key.pub"));
-        isValid = verify(signedData, publicKeyBytes, unsignedData);
-        LOG.info(isValid);
-    }
-
-    public static void main2(String[] args) throws Exception {
-        Security.addProvider(new BouncyCastleProvider());
-
-        //generate and save keys.
-        KeyPair pair = generateKeyPair();
-        PublicKey pub = pair.getPublic();
-        PrivateKey priv = pair.getPrivate();
-        byte[] publicKeyBytes = pub.getEncoded();
-        byte[] privateKeyBytes = priv.getEncoded();
-        Files.write(Paths.get("key.priv"), privateKeyBytes);
-        Files.write(Paths.get("key.pub"), publicKeyBytes);
-
-        String publicKeyHex = Hex.toHexString(publicKeyBytes);
-        LOG.info(publicKeyHex);
-    }
-
-    //todo save hex string instead of bytes? for readability
-    public static void main3(String[] args) throws Exception {
-        Security.addProvider(new BouncyCastleProvider());
-        byte[] privateKeyBytes = Files.readAllBytes(Paths.get("key.priv"));
-        byte[] unsignedData = "SIGN ME".getBytes("UTF-8");
-        byte[] signedData = sign(unsignedData, privateKeyBytes);
-
-        String x = "3059301306072a8648ce3d020106082a8648ce3d03010703420004a0ef13f6a3ce4f3950a5b09e2f78f8aa99b03e6da3420add5649ef2a73cf2c4fed51207cad7b1d13c3e014ff01f004fa31b4755df6d22470995d55ab7533ef7a";
-        byte[] publicFromHex = Hex.decode(x);
-        LOG.info(verify(signedData, publicFromHex, unsignedData));
     }
 
 }
